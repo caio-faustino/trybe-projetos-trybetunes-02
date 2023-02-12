5df22f30cf3a0ab6d47321522aea0e8daf153cc3
caio-faustino <caioexfaustino.pro@gmail.com>
2023-02-11 21:12:55 -0300

    corrigindo lint

--- src/paginas/NotFound.jsx
+++ src/paginas/NotFound.jsx
@@ -2,42 +2,18 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class NotFound extends Component {
-  //  state= {
-  //    boolDidUpdate: false,
-  //  }
-
-  //  componentDidMount() {
-  //
-  //  }
-
-  //  shouldComponentUpdate(nextProps, nextState) {
-  //
-  //    return true;
-  //  }
-
-  //  componentDidUpdate() {
-  //    const { boolDidUpdate } = this.state;
-  //    if (boolDidUpdate) {
-  //      this.setState({boolDidUpdate: false,}, this.func)
-  //    }
-  //  }
-
-  //  componentDidMount() {
-  //
-  //  }
-
   render() {
     const { estadoNome } = this.props;
     return (
-      <div data-testid="page-not-found">
-        NotFound renderizado com o
-        {' '}
-        {estadoNome}
-      </div>
+      <section>
+        <section data-testid="page-not-found">
+          Output NotFound
+          {' '}
+          {estadoNome}
+        </section>
+      </section>
     );
   }
 }
 
-NotFound.propTypes = {
-  estadoNome: PropTypes.string,
-}.isRequired;
+NotFound.propTypes = { estadoNome: PropTypes.string }.isRequired;
